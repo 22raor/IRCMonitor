81c9fa0edf5f85247dc11a80c6da5e3a3817fed8
Rishi Rao <22raor@gmail.com>
2020-03-31 20:18:20 -0400

    Added joining and nick code
    Uses the PC hostname as a nick, adds number for duplicates

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,6 +1,13 @@
 package client;
 
 import org.jibble.pircbot.*;
-public class Client {
+
+public class Client extends PircBot{
+	
+	public Client(String name) {
+		this.setName(name);
+		
+	}
+	
 	
 }
---
+++ src/main/java/client/ClientRunner.java
@@ -0,0 +1,56 @@
+package client;
+
+import java.util.ArrayList;
+
+import org.jibble.pircbot.User;
+
+public class ClientRunner {
+
+	public static String getHostName() {
+		if (System.getProperty("os.name").startsWith("Windows")) {
+		    
+		    return System.getenv("COMPUTERNAME");
+		} else {
+		  
+		    String hostname = System.getenv("HOSTNAME");
+		    if (hostname != null) {
+		       return hostname;
+		    } else {
+		    	return "UnknownHost";
+		    }
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+
+		String channel = "#r3xyz12399";
+		Client client = new Client("hi");
+
+		client.setVerbose(true);
+
+		client.connect("irc.freenode.net");
+
+		client.joinChannel(channel);
+
+		ArrayList<String> users = new ArrayList<String>();
+		
+		
+		User[] current = client.getUsers(channel).clone();
+		
+		String hostname = getHostName();
+		int count = 0;
+		for(User i: current) {
+			if(hostname.equals(i.getNick())) {
+				count++;
+				hostname+=count +"";
+			}
+		}
+		
+		client.changeNick(hostname);
+		
+		
+	}
+
+}
